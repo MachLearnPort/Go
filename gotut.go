68d3ad50bd6c8dc71d2a06c49a2bc12587f11071
MachLearnPort <jeff_schwartzentruber@hotmail.com>
2017-12-13 16:28:44 -0500

    working with value revicers

--- gotut.go
+++ gotut.go
@@ -1,20 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Whoa, GO is neat!")
+const usixteenbitmax float64 = 65535
+const kmhMultiple float64 = 1.60934
+
+type car struct {
+	gasPedal      uint16 // vals 0-65535 (unassigned it 0 and up, where int16 goes to neg)
+	brakePedal    uint16
+	steeringWheel int16
+	topSpeedKMH   float64
 }
 
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About Page")
+// There are two types of methods in GO (value receiver and pointer receivers)
+// This is a method since its using a struct (i.e. class object)
+// This is value reciver
+func (c car) kmh() float64 {
+	return float64(c.gasPedal) * (c.topSpeedKMH / usixteenbitmax)
+}
+
+func (c car) mph() float64 {
+	return float64(c.gasPedal) * (c.topSpeedKMH / usixteenbitmax) / kmhMultiple
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/about", aboutHandler)
-	http.ListenAndServe(":8000", nil)
+	aCar := car{gasPedal: 65000,
+		brakePedal:    0,
+		steeringWheel: 12561,
+		topSpeedKMH:   225.0}
+
+	fmt.Println(aCar.gasPedal)
+	fmt.Println(aCar.kmh())
+	fmt.Println(aCar.mph())
 }
