70aeef722013f37c18c3a615ff5c59391fc292a1
MachLearnPort <jeff_schwartzentruber@hotmail.com>
2017-12-13 17:49:00 -0500

    More
    More

--- gotut.go
+++ gotut.go
@@ -5,6 +5,8 @@ import "fmt"
 const usixteenbitmax float64 = 65535
 const kmhMultiple float64 = 1.60934
 
+var conv float64
+
 type car struct {
 	gasPedal      uint16 // vals 0-65535 (unassigned it 0 and up, where int16 goes to neg)
 	brakePedal    uint16
@@ -15,12 +17,22 @@ type car struct {
 // There are two types of methods in GO (value receiver and pointer receivers)
 // This is a method since its using a struct (i.e. class object)
 // This is value reciver
-func (c car) kmh() float64 {
-	return float64(c.gasPedal) * (c.topSpeedKMH / usixteenbitmax)
+// Makes a copy of the value you pass to it and thus, can mess around without anything touching it, where as pointer recivers will actually change the value
+// Value rev
+// func (c car) kmh() civer more efficent for smaller structs (becuase it has to make a copy), where for larger stucts, pointer revier is easier
+float64 {
+	conv = float64(c.gasPedal) * (c.topSpeedKMH / usixteenbitmax)
+	return conv
 }
 
 func (c car) mph() float64 {
-	return float64(c.gasPedal) * (c.topSpeedKMH / usixteenbitmax) / kmhMultiple
+	// float64(c.gasPedal) is a type conversion
+	return float64(c.gasPedal) * (c.topSpeedKMH / usixteenbitmax / kmhMultiple)
+}
+
+// Pointer receivers
+func (c *car) newTopSpeed(newSpeed float64) {
+	c.topSpeedKMH = newSpeed
 }
 
 func main() {
@@ -30,6 +42,9 @@ func main() {
 		topSpeedKMH:   225.0}
 
 	fmt.Println(aCar.gasPedal)
+	fmt.Println(aCar.kmh())
+	fmt.Println(aCar.mph())
+	aCar.newTopSpeed(500)
 	fmt.Println(aCar.kmh())
 	fmt.Println(aCar.mph())
 }
