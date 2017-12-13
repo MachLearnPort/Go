1e1a7ade4690344a76059805de8c8191b583a177
MachLearnPort <jeff_schwartzentruber@hotmail.com>
2017-12-13 11:02:17 -0500

    First push
    First push

---
+++ gotut.go
@@ -0,0 +1,8 @@
+package main
+
+// Package fmt implements formatted I/O with functions analogous to C's printf and scanf
+import "fmt" // need to use double quotes around imports (all double quotes, unlike python which is single)
+
+func main() {
+	fmt.Println("Welcome to GO") // Very Java based feel
+}
