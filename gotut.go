3bdbbb4e39f081a590638c36bdab1b8aea05c008
MachLearnPort <jeff_schwartzentruber@hotmail.com>
2017-12-13 21:58:06 -0500

    more

--- gotut.go
+++ gotut.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type siteMapIndex struct {
+	Locations []Location `xml:"sitemap"` // []Location is a slice (i.e. [5]type is an array of fixed size (where type is int, float, etc), where as []type is a slice
+	// []Location is a slice of type location. Also, remeber Locations and Location need to have the first letter CAPS to make public
+}
+
+type Location struct {
+	Loc string `xml:"loc"` // Makes Loc the data under the <loc> tag in the XML data
+}
+
+// value receiver, since we are just extracting data and not trying to modify it
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
 func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml") // Get request (_ replaces err handling)
 	bytes, _ := ioutil.ReadAll(resp.Body)                                        // reading the body of the response
-	stringBody := string(bytes)                                                  // Convert to string
-	fmt.Println(stringBody)
-	resp.Body.Close() // Close the resources that made the requests
+	resp.Body.Close()                                                            // Close the resources that made the requests
+
+	var s siteMapIndex
+	xml.Unmarshal(bytes, &s)
+	//fmt.Println(s.Locations)
+
+	for _, Location := range s.Locations { // _ becuase we are not using that placeholder
+		fmt.Printf("\n%s", Location)
+	}
 }
