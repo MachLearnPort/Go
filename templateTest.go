ab3d972cf9d77912e2e3711b2c20aa7c34d2951a
MachLearnPort <jeff_schwartzentruber@hotmail.com>
2017-12-14 14:23:31 -0500

    more

--- templateTest.go
+++ templateTest.go
@@ -1,18 +1,61 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
+type siteMapIndex struct {
+	Locations []string `xml:"sitemap>loc"` // []string is a slice (i.e. [5]type is an array of fixed size (where type is int, float, etc), where as []type is a slice
+	// []Location is a slice of type location. Also, remeber Locations and Location need to have the first letter CAPS to make public
+}
+
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
+
 type NewsAggPage struct {
 	Title string
-	News  string
+	News  map[string]NewsMap
 }
 
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-	p := NewsAggPage{Title: "Amazing News Aggregator", News: "some news"}
+	var s siteMapIndex
+	var n News
+
+	newsMap := make(map[string]NewsMap)
+
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml") // Get request (_ replaces err handling)
+	bytes, _ := ioutil.ReadAll(resp.Body)                                        // reading the body of the response
+	resp.Body.Close()                                                            // Close the resources that made the requests
+	xml.Unmarshal(bytes, &s)
+	// fmt.Println(s.Locations)
+
+	for _, Location := range s.Locations { // _ becuase we are not using that placeholder. Range iterates over our struture
+		resp, _ := http.Get(Location) // Get request (_ replaces err handling)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n) // reading the body of the response
+		//fmt.Println(n.Keywords)
+		// resp.Body.Close()
+
+		//Creating out MAP with titles as our key
+		for idx := range n.Titles {
+			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: newsMap}
 	t, _ := template.ParseFiles("basictemplating.html")
 	t.Execute(w, p)
 }
