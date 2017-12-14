70597b1f0e5951d204971c898326be3bfd3f5615
MachLearnPort <jeff_schwartzentruber@hotmail.com>
2017-12-13 22:06:39 -0500

    Extract News, keywords and locs

--- gotut.go
+++ gotut.go
@@ -2,35 +2,36 @@ package main
 
 import (
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 type siteMapIndex struct {
-	Locations []Location `xml:"sitemap"` // []Location is a slice (i.e. [5]type is an array of fixed size (where type is int, float, etc), where as []type is a slice
+	Locations []string `xml:"sitemap>loc"` // []string is a slice (i.e. [5]type is an array of fixed size (where type is int, float, etc), where as []type is a slice
 	// []Location is a slice of type location. Also, remeber Locations and Location need to have the first letter CAPS to make public
 }
 
-type Location struct {
-	Loc string `xml:"loc"` // Makes Loc the data under the <loc> tag in the XML data
-}
-
-// value receiver, since we are just extracting data and not trying to modify it
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
+type News struct {
+	Titles    []string `xml:"url>news>title`
+	Keyword   []string `xml:"url>news>Keywords"`
+	Locations []string `xml:"url>loc"`
 }
 
 func main() {
+	var s siteMapIndex
+	var n News
+
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml") // Get request (_ replaces err handling)
 	bytes, _ := ioutil.ReadAll(resp.Body)                                        // reading the body of the response
 	resp.Body.Close()                                                            // Close the resources that made the requests
-
-	var s siteMapIndex
 	xml.Unmarshal(bytes, &s)
 	//fmt.Println(s.Locations)
 
-	for _, Location := range s.Locations { // _ becuase we are not using that placeholder
-		fmt.Printf("\n%s", Location)
+	for _, Location := range s.Locations { // _ becuase we are not using that placeholder. Range iterates over our struture
+		resp, _ := http.Get(Location) // Get request (_ replaces err handling)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n) // reading the body of the response
+		// resp.Body.Close()
 	}
 }
