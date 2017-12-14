6de4ae0ccf08e5cbd50c375f6815e04da522ee43
MachLearnPort <jeff_schwartzentruber@hotmail.com>
2017-12-14 18:03:06 -0500

    defer test

--- gotut.go
+++ gotut.go
@@ -1,56 +1,29 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"sync"
+	"time"
 )
 
-type siteMapIndex struct {
-	Locations []string `xml:"sitemap>loc"` // []string is a slice (i.e. [5]type is an array of fixed size (where type is int, float, etc), where as []type is a slice
-	// []Location is a slice of type location. Also, remeber Locations and Location need to have the first letter CAPS to make public
-}
+var wg sync.WaitGroup
 
-type News struct {
-	Titles    []string `xml:"url>news>title"`
-	Keywords  []string `xml:"url>news>keywords"`
-	Locations []string `xml:"url>loc"`
-}
-
-type NewsMap struct {
-	Keyword  string
-	Location string
+func say(s string) {
+	defer wg.Done()
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 100)
+	}
 }
 
 func main() {
-	var s siteMapIndex
-	var n News
+	wg.Add(1)
+	go say("Hey")
+	wg.Add(1)
+	go say("there")
+	//wg.Wait()
 
-	newsMap := make(map[string]NewsMap)
-
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml") // Get request (_ replaces err handling)
-	bytes, _ := ioutil.ReadAll(resp.Body)                                        // reading the body of the response
-	resp.Body.Close()                                                            // Close the resources that made the requests
-	xml.Unmarshal(bytes, &s)
-	// fmt.Println(s.Locations)
-
-	for _, Location := range s.Locations { // _ becuase we are not using that placeholder. Range iterates over our struture
-		resp, _ := http.Get(Location) // Get request (_ replaces err handling)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		xml.Unmarshal(bytes, &n) // reading the body of the response
-		//fmt.Println(n.Keywords)
-		// resp.Body.Close()
-
-		//Creating out MAP with titles as our key
-		for idx := range n.Titles {
-			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
-		}
-	}
-	for idx, data := range newsMap {
-		fmt.Println("\n\n\n", idx)
-		fmt.Println("\n", data.Keyword)
-		fmt.Println("\n", data.Location)
-	}
+	wg.Add(1)
+	go say("Hi")
+	wg.Wait()
 }
